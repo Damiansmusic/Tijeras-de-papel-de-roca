a7ce9d055e9c85dec7e3f31857f61a2b388b771a
Damiansmusic <99570334+Damiansmusic@users.noreply.github.com>
2022-06-05 17:11:53 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,65 +1,64 @@
-var opciones = 
- ["Piedra","Papel","Tijera","Lagarto","Spock"];
+const piedraOpcion = document.getElementById("piedra");
+const papelOpcion = document.getElementById("papel");
+const tijeraOpcion = document.getElementById("tijera");
 
 
-function confirmaJuego(){
-	if(confirm("Confirme que iniciara el juego?.")){
-		iniciarJuego();
-		return true;
-	}else{
-		alert("Si se arrepiente, pulse el boton 'Iniciar Juego'...");
-		return false;
-	}
+/*leemos la entrada del resultado */
+
+const resultadoJuego = document.getElementById("resultado");
+
+//iniciamos el juego
+piedraOpcion.addEventListener("click", () =>{
+    iniciarJuego('piedra');
+});
+
+papelOpcion.addEventListener("click", () =>{
+    iniciarJuego('papel');
+});
+
+tijeraOpcion.addEventListener("click", () =>{
+    iniciarJuego('tijera');
+});
+
+function iniciarJuego(opcion){
+    //movimiento pc
+    const movPC = movimientoPc();
+    //movimiento usuario
+    const movUsuario = opcion;
+    //comparacion de movimiento
+    const comp = compracion(movPC, movUsuario);
+    //resultado
+    if (comp ==1) {
+        resultadoJuego.innerHTML = " usuario elige "+movUsuario + "<br> Pc elige "+ movPC+ "<br> <span class='ganador'>El ganador es usted</span>";
+    }
+    if (comp ==2) {
+        resultadoJuego.innerHTML = " usuario elige "+movUsuario + "<br> Pc elige "+ movPC+ "<br> <span class='perdedor'>El perdedor es usted</span>";
+    }
+    if (comp ==3) {
+        resultadoJuego.innerHTML = " usuario elige "+movUsuario + "<br> Pc elige "+ movPC+ "<br> <span class='empate'>La partida es un empate</span>";
+    }
 }
 
-function iniciarJuego(){
-	var opcionUsuario=prompt("Elige una opcion entre <0-4>\n0: Piedra\n1: Papel\n2: Tijera\n3: Lagarto\n4: Spock\n");
-	var opcionUsuario=parseInt(opcionUsuario);
-	if(isNaN(opcionUsuario)){
-		alert("El valor ingresado no es numero...");
-	}else if(opcionUsuario<0 || opcionUsuario>4){
-		alert("El numero ingresado no corresponde a una de las opciones validas.");
-	}else{
-		var opcionJavascript=aleatorioJavascript(0,4);
-		if (compite(opcionJavascript,opcionUsuario)){
-			alert("Felicitaciones... Acaba de ganar");
-		}else{
-			alert("Lo sentimos, el ganador es Javascript\n Vuelva a intentar...")
-		}
-	}
+function movimientoPc(){
+    const opciones = ['piedra', 'papel', 'tijera'];
+    const random = Math.floor(Math.random()*3);
+    const mov = opciones[random];
+    return (mov);
 }
 
-function aleatorioJavascript(minimo,maximo){
-	var opcionJavascript = Math.floor(Math.random()*(maximo-minimo+1)+minimo);
-	return opcionJavascript;
-}
-
-
-function compite(opcionUsuario,opcionJavascript){
-	alert("Usuario: "+opciones[opcionUsuario] + "    VS    "+"Javascript: " + opciones[opcionJavascript]);
-
-	if(opcionUsuario==opcionJavascript){
-		alert("Empataron!!!...");
-	}else if (opciones[opcionUsuario]=="Piedra"){
-		if(opciones[opcionJavascript]=="Tijera" || opciones[opcionJavascript]=="Lagarto"){
-			var ganador=true;
-		}
-	}else if (opciones[opcionUsuario]=="Papel"){
-		if(opciones[opcionJavascript]=="Piedra" || opciones[opcionJavascript]=="Spock"){
-			var ganador=true;
-		}
-	}else if (opciones[opcionUsuario]=="Tijera"){
-		if(opciones[opcionJavascript]=="Papel" || opciones[opcionJavascript]=="Lagarto"){
-			var ganador=true;
-		}
-	}else if (opciones[opcionUsuario]=="Lagarto"){
-		if(opciones[opcionJavascript]=="Papel" || opciones[opcionJavascript]=="Spock"){
-			var ganador=true;
-		}
-	}else if (opciones[opcionUsuario]=="Spock"){
-		if(opciones[opcionJavascript]=="Piedra" || opciones[opcionJavascript]=="Tijera"){
-			var ganador=true;
-		}
-	}
-	return ganador;
+function compracion(pc, usuario){
+    switch (usuario+pc){
+        case 'piedratijera':
+        case 'papelpiedra':
+        case 'tijerapapel':
+            return 1; //gana
+        case 'tijerapiedra':
+        case 'piedrapapel':
+        case 'papeltijera':
+            return 2; //pierde
+        case 'papelpapel':
+        case 'piedrapiedra':
+        case 'tijeratijera':
+            return 3; //empata
+    }
 }
